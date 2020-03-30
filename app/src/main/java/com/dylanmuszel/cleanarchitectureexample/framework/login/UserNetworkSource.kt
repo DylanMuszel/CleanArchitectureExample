9e4b0b21dc3799c8cf37cb59b51cefb6813e0350
Dylan Muszel <dylan.muszel@wolox.com.ar>
2020-03-30 03:30:18 -0300

    Adding network connection check

--- app/src/main/java/com/dylanmuszel/cleanarchitectureexample/framework/login/UserNetworkSource.kt
+++ app/src/main/java/com/dylanmuszel/cleanarchitectureexample/framework/login/UserNetworkSource.kt
@@ -1,14 +1,23 @@
 package com.dylanmuszel.cleanarchitectureexample.framework.login
 
 import arrow.core.Either
+import com.dylanmuszel.cleanarchitectureexample.framework.core.network.ConnectivityInfo
 import com.dylanmuszel.core.fp.Failure
 import com.dylanmuszel.data.UserSource
 import com.dylanmuszel.domain.UserEntity
 import javax.inject.Inject
 
-class UserNetworkSource @Inject constructor(private val userService: UserService) : UserSource {
+class UserNetworkSource @Inject constructor(
+    private val userService: UserService,
+    private val connectivityInfo: ConnectivityInfo
+) : UserSource {
 
     override suspend fun getUser(email: String, password: String): Either<Failure, UserEntity> {
+
+        if (!connectivityInfo.isOnline) {
+            return Either.Left(Failure.NetworkConnection)
+        }
+
         return try {
             userService.getUser(email, password)
                 .firstOrNull()
